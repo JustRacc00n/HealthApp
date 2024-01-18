436e031bfe848fc240c7a7d212ee4430476b4f54
Bartosz Lewandowski <barteklew123@gmail.com>
2024-01-18 06:47:47 +0100

    Updated main menu with buttons and 3 new activities.

--- app/src/main/java/com/example/szopinz/MainActivity.kt
+++ app/src/main/java/com/example/szopinz/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.example.szopinz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import com.example.szopinz.viewmodel.HealthRecordViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: HealthRecordViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this).get(HealthRecordViewModel::class.java)
+        viewModel = ViewModelProvider(this)[HealthRecordViewModel::class.java]
 
-        submitButton.setOnClickListener {
-            val meal = mealInput.text.toString()
-            val symptoms = symptomsInput.text.toString()
-            val medication = medicationInput.text.toString()
-
-            viewModel.updateHealthRecord(meal, symptoms, medication)
+        val foodButton: Button = findViewById(R.id.foodButton)
+        foodButton.setOnClickListener {
+            val intent = Intent(this, FoodActivity::class.java)
+            startActivity(intent)
         }
+
+        val symptomsButton: Button = findViewById(R.id.symptomsButton)
+        symptomsButton.setOnClickListener {
+            val intent = Intent(this, SymptomsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val medsButton: Button = findViewById(R.id.medsButton)
+        medsButton.setOnClickListener {
+            val intent = Intent(this, MedsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
 
--- app/src/main/java/com/example/szopinz/viewmodel/HealthRecordViewModel.kt
+++ app/src/main/java/com/example/szopinz/viewmodel/HealthRecordViewModel.kt
@@ -2,6 +2,7 @@ package com.example.szopinz.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
+import com.example.szopinz.model.HealthRecord
 
 class HealthRecordViewModel : ViewModel() {
     val healthRecord = MutableLiveData<HealthRecord>()
